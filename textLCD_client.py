d029ad39a5e218d3b329d584ca1f229289d64021
Minh Le <91708157+leminh02@users.noreply.github.com>
2022-12-06 15:07:37 +0800

    Add files via upload

---
+++ textLCD_client.py
@@ -0,0 +1,30 @@
+from time import sleep
+import erpc
+from text_lcd import *
+import sys
+from array import *
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("Usage: python led_test_client.py COM5")
+        exit()
+
+    # Initialize all erpc infrastructure
+    xport = erpc.transport.SerialTransport(sys.argv[1], 9600)
+    client_mgr = erpc.client.ClientManager(xport, erpc.basic_codec.BasicCodec)
+    client = client.TextLCDServiceClient(client_mgr)
+
+    # Write character on the connected erpc server
+    myNum = array('B',[49,50,51,52,53,54])
+    #myNum = ['L','E','M','I','N','H']
+    client.location(3,0)
+    for i1 in range(0,2):
+       client.enterChar(myNum[i1])
+       sleep(1)
+    client.location(3,1)
+    for i2 in range(2,6):
+       client.enterChar(myNum[i2])
+       sleep(1)
+
+    
